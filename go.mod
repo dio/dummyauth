de50eea54fd9e7efd698adcc2ae0a906c5420b5a
Dhi Aurrahman <dio@tetrate.io>
2019-09-02 20:00:27 +0700

    Update
    Signed-off-by: Dhi Aurrahman <dio@tetrate.io>

--- go.mod
+++ go.mod
@@ -1,10 +1,10 @@
-module dummyauth
+module github.com/dio/dummyauth
 
 go 1.12
 
 require (
 	github.com/envoyproxy/go-control-plane v0.8.6
-	github.com/gogo/googleapis v1.2.0
-	github.com/golang/protobuf v1.3.2
-	google.golang.org/grpc v1.23.0
+	github.com/gogo/protobuf v1.3.0
+	google.golang.org/grpc v1.19.1
+	istio.io/gogo-genproto v0.0.0-20190731221249-06e20ada0df2
 )
--- pkg/auth/auth.go
+++ pkg/auth/auth.go
@@ -3,14 +3,16 @@ package auth
 import (
 	"context"
 	"log"
+	"os"
 	"time"
 
 	v2 "github.com/envoyproxy/go-control-plane/envoy/service/auth/v2"
-	"github.com/gogo/googleapis/google/rpc"
-	"github.com/golang/protobuf/jsonpb"
+	"github.com/gogo/protobuf/jsonpb"
+	google_rpc "istio.io/gogo-genproto/googleapis/google/rpc"
 )
 
 type server struct {
+	fail      bool
 	marshaler jsonpb.Marshaler
 }
 
@@ -18,7 +20,8 @@ var _ v2.AuthorizationServer = &server{}
 
 // New creates a new authorization server.
 func New() v2.AuthorizationServer {
-	return &server{}
+	_, fail := os.LookupEnv("FAIL")
+	return &server{fail: fail}
 }
 
 func (s *server) Check(ctx context.Context, req *v2.CheckRequest) (*v2.CheckResponse, error) {
@@ -27,11 +30,14 @@ func (s *server) Check(ctx context.Context, req *v2.CheckRequest) (*v2.CheckResp
 	log.Println(str)
 
 	time.Sleep(300 * time.Millisecond)
+	code := int32(google_rpc.OK)
+	if s.fail {
+		code = int32(google_rpc.UNAUTHENTICATED)
+	}
 
 	return &v2.CheckResponse{
-		Status: &rpc.Status{
-			Code:    int32(rpc.OK),
-			Message: "",
+		Status: &google_rpc.Status{
+			Code: code,
 		},
 	}, nil
 }
